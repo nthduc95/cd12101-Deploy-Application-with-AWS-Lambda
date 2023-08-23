a5996424657ad07a8e448679c436d9c57dea5e9a
nthduc95 <nthduc95@gmail.com>
2023-08-23 14:42:29 +0700

    add env client

--- starter/backend/src/lambda/auth/auth0Authorizer.mjs
+++ starter/backend/src/lambda/auth/auth0Authorizer.mjs
@@ -4,7 +4,7 @@ import { createLogger } from '../../utils/logger.mjs'
 
 const logger = createLogger('auth')
 
-const jwksUrl = 'https://test-endpoint.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-tyxip2be0n264sh4.us.auth0.com/.well-known/jwks.json'
 
 export async function handler(event) {
   try {
